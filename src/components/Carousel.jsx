625c4cf38552f73f0cd26fa786a11326cc0b6ecb
mistydelacruz <misty.dela.cruz1@gmail.com>
2024-05-14 12:21:17 -1000

    Carousel is working

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -18,13 +18,19 @@ function Carousel() {
     return (
         <>
             <div className="carousel">                               {/*Divider for the logo*/}
-                <div className="carousel-inner" style={{backgroundImage: `url(${images[currImg].img})`}}>   {/*Divider for the inner carousel*/}
-                    <div className="carousel-left">                  {/*Divider for the left arrow icon*/}
+                <div className="carousel-inner" style={{backgroundImage: `url(${images[currImg].img})`}}>       {/*Divider for the inner carousel*/}
+                    <div className="carousel-left" 
+                    onClick={() => 
+                    {currImg > 0 && setCurrImg(currImg - 1);}}>                                 {/*Divider for the left arrow icon, checks if currImg > 0 AND currImg decreases by 1*/}
+                        <KeyboardArrowLeftIcon style={{fontSize: 30}}/>
                     </div>
                     <div className="carousel-center">
-
+                        <p>caption for pics</p>
                     </div>
-                    <div className="carousel-right">                 {/*Divider for the right arrow icon*/}
+                    <div className="carousel-right" 
+                    onClick={() => 
+                    {currImg < (images.length - 1) && setCurrImg(currImg + 1);}}>                 {/*Divider for the right arrow icon, checks if currImg < images.length - 1 AND currImg increases by 1*/}
+                        <KeyboardArrowRightIcon style={{fontSize: 30}}/>
                     </div>
                 </div>
             </div>
